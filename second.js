ca1e12db3c99a51f819cb490d5f91cc690903d43
Evgenij Ryabko <jeniaryanko@mail.ru>
2022-12-05 15:17:55 +0300

    NewVersion

--- second.js
+++ second.js
@@ -1,5 +1,5 @@
 const str = 'Hello my little word, dab, dab';
 
-console.log(str.filter((arg)=>{if (arg == 'l') return arg;}).length);
-
-//Сделать через reduce()
+console.log([...str].reduce((Amount, char)=>{
+    return Amount+=char==='l';
+}, 0));
--- third.js
+++ third.js
@@ -1,5 +1,3 @@
 let data = [1, 2, 3, false, undefined, 4, undefined, false, undefined];
 
-console.log(data.filter((arg)=>{if (typeof(arg)==="number") return arg;}));
-
-//Сделать через filter, но проще
+console.log(data.filter((arg)=>{return typeof(arg)==="number"&&arg;}));
